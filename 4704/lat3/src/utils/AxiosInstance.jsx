8a786283f54fae88c3899c68456ff8ffa813825c
DannyNaufal25 <dannyprof2004@gmail.com>
2025-12-23 14:29:21 +0700

    Fix: Update AxiosInstance to use environment variable for API URL

--- 4704/lat3/src/utils/AxiosInstance.jsx
+++ 4704/lat3/src/utils/AxiosInstance.jsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const AxiosInstance = axios.create({
-  baseURL: "http://localhost:3001", // alamat json-server
+  baseURL: import.meta.env.VITE_API_BASE_URL || "http://localhost:3001",
   headers: {
     "Content-Type": "application/json",
   },
